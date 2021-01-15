acd80a624a5087543f030f8d00f3aa4201d8b945
yusoojeong <yusj1231@gmail.com>
2021-01-15 14:23:32 +0900

    [Edit] app name
    Change due to typos during creation
    
    Signed-off-by: yusoojeong <yusj1231@gmail.com>

--- lotto/lotto/settings.py
+++ lotto/lotto/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # app
-    'unnuity',
+    'annuity',
 ]
 
 MIDDLEWARE = [
--- lotto/unnuity/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UnnuityConfig(AppConfig):
-    name = 'unnuity'
