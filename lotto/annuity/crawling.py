bd45f6adf476f7d2e57743d54ec21cde6b1e0b6a
yusoojeong <yusj1231@gmail.com>
2021-02-22 18:26:55 +0900

    [Add] README
    Signed-off-by: yusoojeong <yusj1231@gmail.com>

--- lotto/annuity/crawling.py
+++ lotto/annuity/crawling.py
@@ -36,15 +36,30 @@ def crawler(n):
     for i in range(6):
         data['num' + str(i+1)] = int(nums[i].text)
 
+    bonus = res.select(
+        '#after720 > div:nth-child(4) > div > span > span'
+    )
+
+    for i in range(6):
+        data['bonus' + str(i+1)] = int(bonus[i].text)
+
     print(data)
 
     return data
 
 def basic_data():
 
-    for n in range(1, 5 + 1):
-        crawler(n)
+    # navertv.csv파일을 쓰기모드(w)로 열어줍니다.
+    f = open("lotto.csv", "w")
+    # 헤더 추가하기
+    f.write("회차, 조, 번호1, 번호2, 번호3, 번호4, 번호5, 번호6, , 보너스1, 보너스2, 보너스3, 보너스4, 보너스5, 보너스6" + "\n")
 
-
+    for n in range(1, 38 + 1):
+        res = crawler(n)
+        f.write(str(res['no']) + "," + str(res['jo']) + "," + str(res['num1']) + "," + str(res['num2']) + "," + str(res['num3']) +
+        "," + str(res['num4']) + "," + str(res['num5']) + "," + str(res['num6']) + "," + "" + "," + str(res['bonus1']) + "," + 
+        str(res['bonus2']) + "," + str(res['bonus3']) + "," + str(res['bonus4']) + "," + str(res['bonus5']) + "," + str(res['bonus6']) + "\n")
+    
+    f.close()
 
 basic_data()
